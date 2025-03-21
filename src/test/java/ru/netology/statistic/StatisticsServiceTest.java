3f9e8de82360e2ef88637b9954ffa9f2485998c1
Pavel Lebedev <p.lebedev8888@gmail.com>
2025-03-21 14:58:20 +0300

    initial_v2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInTheMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 100, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 100;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
